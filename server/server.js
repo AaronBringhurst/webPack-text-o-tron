c2786754ad5450b72095e7b870729f4ad8cb1f07
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-07-16 20:20:15 -0600

    message!!!!

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// /app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/index.html"));
-// });
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
