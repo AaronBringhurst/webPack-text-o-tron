6406e94233c04332640937a7b9361f704c115545
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-07-16 21:13:21 -0600

    i dont have a clue, i not sure anyone has a clue

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("/..client/dist"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,14 +9,4 @@ app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-server.on('error', (error) => {
-    if (error.code === 'EADDRINUSE') {
-        console.log(`Port ${PORT} is already in use, trying another port.`);
-        const newPort = PORT + 1; // Increment port number
-        server.listen(newPort, () => console.log(`Server running on port ${newPort}`));
-    } else {
-        console.log('Error starting server:', error);
-    }
-});
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
