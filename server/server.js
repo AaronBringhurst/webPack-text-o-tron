85d2a618aa21ce5562349e43cd9ad215c7c491d1
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-07-16 21:05:01 -0600

    dfaewf e

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("/..client/dist"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,4 +9,14 @@ app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+server.on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+        console.log(`Port ${PORT} is already in use, trying another port.`);
+        const newPort = PORT + 1; // Increment port number
+        server.listen(newPort, () => console.log(`Server running on port ${newPort}`));
+    } else {
+        console.log('Error starting server:', error);
+    }
+});
